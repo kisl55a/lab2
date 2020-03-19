e948423a31fd774bdf85766a8e8add8f5716992a
Dmitrii <kislenko1910@mail.ru>
2020-03-19 20:01:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final int[] clicks = {0};
+        final Button gameButton = new Button(this);
+        gameButton.setText("Hello, I'm the button. Press me!");
+        setContentView(gameButton);
+//        setContentView(R.layout.activity_main);
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                gameButton.setText("You have clicked the button "+ clicks[0] +" times");
+                clicks[0] = clicks[0] + 1;
+            }
+        });
+    }
+
+}
